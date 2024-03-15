56be882d3916b267a8d2f07377d93943d0e333ad
PetterMartin <petter.aa@icloud.com>
2024-03-15 11:40:25 +0100

    smaller text on footer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,32 +11,32 @@ const Navbar = () => {
     <div className="hidden lg:block border-2 border-gray-200">
       <div className="container">
         <div className="flex w-fit gap-4 ms-2 font-medium py-2">
-          <div className="flex gap-12 items-center bg-gray-800 py-2 px-3 text-white text-sm rounded-lg">
+          <div className="flex gap-12 items-center bg-gray-800 py-1.5 px-3 text-white text-sm rounded-lg">
             <div className="flex gap-2 items-center">
               <RxHamburgerMenu /> CATEGORIES{" "}
             </div>
             <IoIosArrowDown />
           </div>
           <Link
-            className="flex gap-2 items-center bg-gray-50 text-gray-700 py-2 px-3 text-sm border-2 border-gray-300 rounded-lg font-semibold cursor-pointer hover:bg-gray-100"
+            className="flex gap-2 items-center bg-gray-50 text-gray-700 py-1.5 px-3 text-sm border-2 border-gray-300 rounded-lg font-semibold cursor-pointer hover:bg-gray-100"
             href="#"
           >
             <MdOutlineHeadset size={20} /> Electronics
           </Link>
           <Link
-            className="flex gap-2 items-center bg-gray-50 text-gray-700 py-2 px-3 text-sm border-2 border-gray-300 rounded-lg font-semibold cursor-pointer hover:bg-gray-100"
+            className="flex gap-2 items-center bg-gray-50 text-gray-700 py-1.5 px-3 text-sm border-2 border-gray-300 rounded-lg font-semibold cursor-pointer hover:bg-gray-100"
             href="#"
           >
             <LiaLaptopSolid size={20}/> Computer & Laptop
           </Link>
           <Link
-            className="flex gap-2 items-center bg-gray-50 text-gray-700 py-2 px-3 text-sm border-2 border-gray-300 rounded-lg font-semibold cursor-pointer hover:bg-gray-100"
+            className="flex gap-2 items-center bg-gray-50 text-gray-700 py-1.5 px-3 text-sm border-2 border-gray-300 rounded-lg font-semibold cursor-pointer hover:bg-gray-100"
             href="#"
           >
             <BsPhone size={20}/> Phone & Tablet
           </Link>
           <Link
-            className="flex gap-2 items-center bg-gray-50 text-gray-700 py-2 px-3 text-sm border-2 border-gray-300 rounded-lg font-semibold cursor-pointer hover:bg-gray-100"
+            className="flex gap-2 items-center bg-gray-50 text-gray-700 py-1.5 px-3 text-sm border-2 border-gray-300 rounded-lg font-semibold cursor-pointer hover:bg-gray-100"
             href="#"
           >
             <BsUsbC size={20}/> Harddisk & Flashdisk
